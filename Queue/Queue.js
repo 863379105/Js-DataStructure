d9c46b45f08f01cd29305ceb519d85beaabb5d87
lb <863379105@qq.com>
2021-12-19 01:10:12 +0800

    完成队列数据结构

---
+++ Queue/Queue.js
@@ -0,0 +1,32 @@
+function Queue() {
+  this.items = {}
+  this.lowestCount = 0
+  this.length = 0
+}
+Queue.prototype.enqueue = function(ele) {
+  this.items[this.length] = ele
+  this.length++
+}
+Queue.prototype.dequeue = function() {
+  if(this.isEmpty()) return undefined
+  let ele = this.items[this.lowestCount]
+  delete this.items[this.lowestCount]
+  this.length--
+  this.lowestCount++
+  return ele
+}
+Queue.prototype.isEmpty = function() {
+  return this.length === 0
+}
+Queue.prototype.peek = function() {
+  if(this.isEmpty()) return undefined
+  return this.items[this.lowestCount]
+}
+Queue.prototype.size = function() {
+  return this.length
+}
+Queue.prototype.clear = function() {
+  this.items = {}
+  this.lowestCount = 0
+  this.length = 0
+}

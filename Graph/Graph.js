c5be80dc7492923e8f97dc9d4aab4538a2c2db5b
lb <863379105@qq.com>
2022-01-13 00:02:48 +0800

    整理代码

--- Graph/Graph.js
+++ Graph/Graph.js
@@ -135,31 +135,29 @@ const shortestPath = (graph,v,w) => {
   return path
 }
 
-// -------------
-const graph = new Graph()
-const vertices = ["A","B","C","D","E","F","G","H","I"]
-vertices.map(v => {
-  graph.addVertex(v)
-})
-graph.addEdge('A','B')
-graph.addEdge('A','C')
-graph.addEdge('A','D')
-graph.addEdge('C','D')
-graph.addEdge('C','G')
-graph.addEdge('D','G')
-graph.addEdge('D','H')
-graph.addEdge('B','E')
-graph.addEdge('B','F')
-graph.addEdge('E','I')
-// -------------------
-
+// ------------- TEST Sample
+// const graph = new Graph()
+// const vertices = ["A","B","C","D","E","F","G","H","I"]
+// vertices.map(v => {
+//   graph.addVertex(v)
+// })
+// graph.addEdge('A','B')
+// graph.addEdge('A','C')
+// graph.addEdge('A','D')
+// graph.addEdge('C','D')
+// graph.addEdge('C','G')
+// graph.addEdge('D','G')
+// graph.addEdge('D','H')
+// graph.addEdge('B','E')
+// graph.addEdge('B','F')
+// graph.addEdge('E','I')
 // breadthFirstSearch(graph,"A",(v) => {
 //   console.log(v);
 // })
 
+// vertices.map(v => {
+//   console.log(shortestPath(graph,'A',v));
+// })
+// -------------------
 
 
-
-vertices.map(v => {
-  console.log(shortestPath(graph,'A',v));
-})
